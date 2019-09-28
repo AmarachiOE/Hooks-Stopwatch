4737254e8df260e47d1da27509b5bb206f338dbd
AmarachiOE <ao.ejiawoko@gmail.com>
2019-09-28 15:46:03 -0700

    dynamic button text

--- hooks-stopwatch/src/App.js
+++ hooks-stopwatch/src/App.js
@@ -8,18 +8,38 @@ function App() {
     "width": "200px",
     "height": "50px",
     "background": "indigo",
-    "font-size": "25px",
-    "border-radius": "5px",
-    "font-weight": "700",
+    "fontSize": "25px",
+    "borderRadius": "5px",
+    "fontWeight": "700",
     "color": "#D175F0",
   }
-  const [count, setCount] = useState(0)
+  const [lapse, setLapse] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  const startStop = () => {
+    if (running) {
+      clearInterval();
+    } else {
+      const startTime = Date.now() - lapse;
+      setInterval(() => {
+        setLapse(Date.now() - startTime)
+      }, 0)
+    }
+    setRunning(!running);
+  }
+
+  const clear = () => {
+    setLapse(0)
+  }
+  
+  console.log("Running", running)
+
   return (
     <div className="App">
       <h2>STOPWATCH</h2>
-      <h1>{count} ms</h1>
-      <button style={buttonStyle} onClick={() => {setCount(count + 1)}}>START</button>
-      <button style={buttonStyle}>CLEAR</button>
+      <h1>{lapse} ms</h1>
+      <button style={buttonStyle} onClick={startStop}>{running ? "STOP" : "START"}</button>
+      <button style={buttonStyle} onClick={clear}>CLEAR</button>
 
     </div>
   );
